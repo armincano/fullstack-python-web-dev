2b5efd0d9acbb27c3210ce8065c2c6561a6d4ca5
Armin Cano <arminescano@gmail.com>
2024-09-30 23:48:27 -0300

    feat: add models to portfolio django project

--- m6-django/misc/practica_django/book/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-app_name="book"
-
-urlpatterns = [
-    path('commands/', views.commands, name='commands'),
-    path('', views.index, name='index'),
-]
--- m6-django/misc/practica_django/book/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def commands(request):
-    return render(request, "book/commands.html")
-
-def index(request):
-    return render(request, "book/index.html")
