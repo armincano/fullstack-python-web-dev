cbc7d3a5334bdb5fb0e034c920d4f090e9429235
Armin Cano <arminescano@gmail.com>
2024-10-01 23:26:18 -0300

    feat: add is a palindrome exercise

--- m6-django/exercise/palindrome/urls.py
+++ m6-django/exercise/palindrome/urls.py
@@ -3,4 +3,5 @@ from palindrome import views
 
 urlpatterns = [
     path("", views.home, name='home'),
+    path("<str:word>/", views.is_palindrome, name="is_palindrome")
 ]
--- m6-django/exercise/palindrome/views.py
+++ m6-django/exercise/palindrome/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "palindrome/home.html")
+
+def is_palindrome(request, word):
+    is_palindrome = word.lower().replace(" ", "") == word[::-1].lower().replace(" ", "")
+    context = {"word": word, "is_palindrome": is_palindrome}
+    return render(request, "palindrome/is_palindrome.html", context)
