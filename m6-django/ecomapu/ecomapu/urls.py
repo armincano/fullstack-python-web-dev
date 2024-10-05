1d4d8abf801b1f7fed2d13df7b11402da09d836e
Armin Cano <arminescano@gmail.com>
2024-10-05 19:43:28 -0300

    feat: modify STATICFILES_DIRS setting to load static files

--- m6-django/ecomapu/ecomapu/urls.py
+++ m6-django/ecomapu/ecomapu/urls.py
@@ -25,6 +25,3 @@ urlpatterns = [
     path('', views.index, name="home"),
     path('menu/', include("menu.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
