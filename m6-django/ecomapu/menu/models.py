1f8144159781260ec253402826fcf218b4b56be7
Armin Cano <arminescano@gmail.com>
2024-10-04 21:48:12 -0300

    feat: add MenuItem model; migrate models; add static and media settings

--- m6-django/ecomapu/menu/models.py
+++ m6-django/ecomapu/menu/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class MenuItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='images/')
