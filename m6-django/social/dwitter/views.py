51b26d92c6a737d601cffbba984c1c8ea97e0254
Armin Cano <arminescano@gmail.com>
2024-10-08 16:58:47 -0300

    feat: add social 'dwitter' exercise using django form

--- m6-django/social/dwitter/views.py
+++ m6-django/social/dwitter/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import DweetForm
 from .models import Profile
 
 
 def dashboard(request):
-    return render(request, "dwitter/dashboard.html")
+    form = DweetForm(request.POST or None)
+    if request.method == "POST":
+        form = DweetForm(request.POST)
+        if form.is_valid():
+            dweet = form.save(commit=False)
+            dweet.user = request.user
+            dweet.save()
+            return redirect("dwitter:dashboard")
+    context = {"form": form}
+    return render(request, "dwitter/dashboard.html", context)
 
 
 def profile_list(request):
