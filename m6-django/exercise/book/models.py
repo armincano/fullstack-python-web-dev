20cac5db7e7eb77499e955094c9031d05bc83b75
Armin Cano <arminescano@gmail.com>
2024-10-19 18:43:16 -0300

    feat: add 3 permissions to Book model

--- m6-django/exercise/book/models.py
+++ m6-django/exercise/book/models.py
@@ -5,5 +5,12 @@ class Book(models.Model):
     author = models.CharField(max_length=150)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
+    class Meta:
+        permissions = (
+            ('development', 'Can do development'),
+            ('scrum_master', 'Can do scrum master'),
+            ('product_owner', 'Can do product owner'),
+        )
+    
     def __str__(self):
         return self.title
