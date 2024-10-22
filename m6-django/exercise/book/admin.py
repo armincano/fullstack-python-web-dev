48c4e8c963da9591031ec5f5e7c67047fe218d89
Armin Cano <arminescano@gmail.com>
2024-10-21 22:24:07 -0300

    feat: add 'created_at' and 'updated_at' fields to Book model;  add filter to django admin page

--- m6-django/exercise/book/admin.py
+++ m6-django/exercise/book/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
     list_display = ('title', 'author', 'price')
     search_fields = ('title', 'author')
+    list_filter = ('price','updated_at')
 
 admin.site.register(Book, BookAdmin)
--- m6-django/exercise/book/models.py
+++ m6-django/exercise/book/models.py
@@ -4,8 +4,12 @@ class Book(models.Model):
     title = models.CharField(max_length=150)
     author = models.CharField(max_length=150)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
+        verbose_name = "Libro"
+        verbose_name_plural = "Libros"
         permissions = (
             ('development', 'Can do development'),
             ('scrum_master', 'Can do scrum master'),
