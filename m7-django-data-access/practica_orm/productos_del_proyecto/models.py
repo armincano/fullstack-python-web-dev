a1d0b9731ecd7489f33e03e533826c05deb61a3a
Armin Cano <arminescano@gmail.com>
2024-11-08 00:20:18 -0300

    feat: add models and relational field to practica_orm

--- m7-django-data-access/practica_orm/productos_del_proyecto/models.py
+++ m7-django-data-access/practica_orm/productos_del_proyecto/models.py
@@ -4,6 +4,13 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=8, decimal_places=2)
+    fabricante = models.ForeignKey('Fabricante', on_delete=models.CASCADE, related_name='productos', null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Fabricante(models.Model):
+    nombre = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nombre
